77a12dc678ab76966267a307c24af6d2c66c22bf
Xiao <smallslava@yahoo.co.uk>
2017-04-09 23:05:31 +0300

    correct function for calcuting matrix inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,7 @@
+#computes the inverse of a matrix, but first it checks if any cached solution exists
+
 makeCacheMatrix <- function(x = matrix()) {
+        #this function creates a special vector for keeping calculated inverse of a matrix
         s <- NULL
         set <- function(y) {
                 x <<- y
@@ -13,6 +16,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 cacheSolve <- function(x, ...) {
+        #this function checks if the solution for the input matrix exists in cache
+        #if not, it completes the calculation and stores the inverse of the current input matrix in cache and print the result
         s <- x$getsolve()
         if(!is.null(s)) {
                 message("getting cached data")
